435d6858988a0a86484a931463ecd12e4d94a1cf
kumagaias <14195829+kumagaias@users.noreply.github.com>
2025-10-19 17:05:27 +0900

    fix: amplify settings

--- front/next.config.js
+++ front/next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',  // Amplify Hosting 用
   env: {
     // ローカル開発時のCognito設定を強制
     AWS_ENDPOINT_URL_COGNITO_IDP: process.env.NODE_ENV === 'development' ? 'http://localhost:9229' : undefined,
   },
-  experimental: {
-    serverComponentsExternalPackages: ['aws-amplify'],
-  },
+  serverExternalPackages: ['aws-amplify'],
 }
 
 module.exports = nextConfig
